e5587a2e06f9b8311ac11fe0e2ef3f2e649f2dcf
Richard Barnes <rbarnes@umn.edu>
2019-07-05 10:40:00 -0700

    Cleaned up GTFS parsing code

--- src/port_to_mesa.py
+++ src/port_to_mesa.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 from functools import partial
 import pandas as pd
 import partridge as ptg
@@ -9,7 +10,7 @@ import sys
 wgs_to_aea = partial(
     pyproj.transform,
     pyproj.Proj(init='epsg:4326'), # source coordinate system
-    pyproj.Proj(init='ESRI:102003')) # destination coordinate system
+    pyproj.Proj(init='esri:102003')) # destination coordinate system
     #Converts to a US Contiguous Albert Equal Area projection. This fails on pyproj>2.0. 
 
 def GetGeoDistanceFromLineString(line_string):
@@ -19,32 +20,98 @@ def GetGeoDistanceFromLineString(line_string):
 
 
 
-if len(sys.argv)!=2:
-  print("Syntax: {0} <GTFS File>".format(sys.argv[0]))
-  return -1;
+if len(sys.argv)!=3:
+  print("Syntax: {0} <GTFS File> <Output>".format(sys.argv[0]))
+  sys.exit(-1)
 
 feed_file = sys.argv[1]
 
+feed_file = "data/gtfs_minneapolis.zip"
+
+#######################
+#Filter for service ids
+
 # find the busiest date
 gtfs = ptg.load_geo_feed(feed_file)
-date, service_ids = ptg.read_busiest_date(feed_file) # this returns multiple days, does this cause overlap in trips?
+date, service_ids = ptg.read_busiest_date(feed_file)
 
-filtered_trips = gtfs.trips[gtfs.trips.service_id.isin(service_ids)]
-trip_stops = filtered_trips.merge(gtfs.stop_times, left_on='trip_id', right_on='trip_id')
-trip_stops = trip_stops.sort_values(['trip_id','route_id','service_id','stop_sequence'])
+print("Service id chosen = {0}".format(service_ids))
 
-trips = trip_stops.iloc[0,0:11].to_frame().transpose() # create a final "trips" df, using only needed cols. 
-# Should probaly use col labels instead of index, as there are optional col names that could be included in other feeds. 
-trips = trips.drop(0) # I couldnt figure out how to just take a slice of the header row w/out also taking at least the first row. Please advise.
+#Select the service ids from that date. Note that trip_ids are still unique
+#because they include service_ids as a substring
+trips = gtfs.trips[gtfs.trips.service_id.isin(service_ids)]
 
-ts_by_tid = trip_stops.groupby(by='trip_id') # group trips by trip_id and then find origin & destination stop_ids
-for name, group in ts_by_tid:
-  trips = trips.append(group[0:1])
-  trips['origin_stop'] = group['stop_id'].iloc[0]
-  trips['destination_stop'] = group['stop_id'].iloc[-1]
-# there must be a better way than a loop for this?
+#Clean up stops data
+gtfs.stops['lat'] = gtfs.stops.geometry.y
+gtfs.stops['lng'] = gtfs.stops.geometry.x
 
-gtfs.shapes['distances'] = gtfs.shapes['geometry'].map(GetGeoDistanceFromLineString)
-trips = gtfs.shapes[['shape_id','distances']].merge(trips)
-# this merge does not work. halp!
-trips.to_csv('/home/nclarke/winhome/repos/RISE/RISE Bus/trips.csv')
+#Combine trips with stop data
+trips = trips.merge(gtfs.stop_times, left_on='trip_id', right_on='trip_id')
+
+#Filter trips to only regularly scheduled stops
+trips = trips[trips.pickup_type.astype(int)==0]
+trips = trips[trips.drop_off_type.astype(int)==0]
+
+trips = trips.sort_values(['trip_id','route_id','service_id','stop_sequence'])
+
+#TODO: Maybe concatenate trip_id with a service_id and route_id and direction_id to ensure it is unique?
+
+#Drop unneeded columns
+trips = trips.drop(columns=[
+  'wheelchair_accessible',
+  'trip_headsign',
+  'pickup_type',
+  'drop_off_type', 
+  'service_id',       #Trip ids include this as a substring
+  'direction_id',
+  'route_id',
+  'block_id',
+  'stop_sequence'     #This is held by the sorted order
+])
+
+#TODO: block_id is supposed to indicate continuous travel by a *single vehicle*
+#and, thus, might provide a good way of simplifying the problem
+
+#We need to get information about when each trip begins and ends, so we sort and
+#then group by trip. Each group is then ordered such that its first and last
+#entries contain the needed information.
+first_stops = trips.groupby(by='trip_id').first().reset_index()
+last_stops  = trips.groupby(by='trip_id').last().reset_index()
+
+first_stops = first_stops.merge(gtfs.stops[['stop_id','lat','lng']], how='left', on='stop_id')
+last_stops  = last_stops.merge (gtfs.stops[['stop_id','lat','lng']], how='left', on='stop_id')
+
+first_stops = first_stops.rename(index=str, columns={
+  "shape_id":       "start_shape_id",
+  "arrival_time":   "start_arrival_time",
+  "departure_time": "start_departure_time",
+  "stop_id":        "start_stop_id",
+  "lat":            "start_lat",
+  "lng":            "start_lng"
+})
+
+last_stops = last_stops.rename(index=str, columns={
+  "shape_id":       "end_shape_id",
+  "arrival_time":   "end_arrival_time",
+  "departure_time": "end_departure_time",
+  "stop_id":        "end_stop_id",
+  "lat":            "end_lat",
+  "lng":            "end_lng"
+})
+
+trips = first_stops.merge(last_stops, on="trip_id")
+
+#Clean up shape ids
+assert (trips['start_shape_id']==trips['end_shape_id']).all()
+trips = trips.drop(columns=["end_shape_id"])
+trips = trips.rename(index=str, columns={"start_shape_id": "shape_id"})
+
+#Merge in distances
+gtfs.shapes['distance'] = gtfs.shapes['geometry'].map(GetGeoDistanceFromLineString)
+trips = trips.merge(gtfs.shapes[['shape_id', 'distance']], how='left', on='shape_id')
+trips = trips.drop(columns='shape_id')
+
+trips = trips.sort_values(['start_departure_time'])
+
+#Output
+trips.drop(columns=['trip_id']).to_csv(sys.argv[2], index=False)
